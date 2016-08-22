1e2b614d3dda88e5349a99f1ef67da6edbb166c4
diegog <diego.galico@globant.com>
2016-08-22 18:26:04 -0300

    Crittercism integration

--- app/src/main/java/com/checkfood/singluten/AndroidApplication.java
+++ app/src/main/java/com/checkfood/singluten/AndroidApplication.java
@@ -2,6 +2,9 @@ package com.checkfood.singluten;
 
 import android.app.Application;
 
+import com.checkfood.singluten.domain.Constants;
+import com.crittercism.app.Crittercism;
+
 import timber.log.Timber;
 import timber.log.Timber.DebugTree;
 
@@ -9,7 +12,8 @@ public class AndroidApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        //initiate Crittercism
+        Crittercism.initialize(this, Constants.CRITTERCISM_API_KEY);
         // initiate Timber
         Timber.plant(new DebugTree());
     }
---
+++ app/src/main/java/com/checkfood/singluten/domain/Constants.java
@@ -0,0 +1,8 @@
+package com.checkfood.singluten.domain;
+
+/**
+ * Created by diego.galico on 22/08/2016.
+ */
+public class Constants {
+    public static final String CRITTERCISM_API_KEY = "72d68d0e0f494f5e9f20e07ced05017500555300";
+}
